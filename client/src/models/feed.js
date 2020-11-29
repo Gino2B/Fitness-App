a6c1f548389d78319ef7fe539d1a79ad99d04fbd
Gino <genebaybay@yahoo.com>
2020-11-28 21:28:57 -0500

    client side changes

--- client/src/models/feed.js
+++ client/src/models/feed.js
@@ -2,32 +2,16 @@
     Feed Model. The raw data for the feed display.
     Currently mocked at the client side.
 */
-export const posts = [
-    {
-        url: '',
-        message: '',
-        owner: {
-            name: 'John Smith',
-            handle: 'johnsmith',
-            profile: '',
-        }
-    },
-    {
-        url: '',
-        message: '',
-        owner: {
-            name: 'John Smith',
-            handle: 'johnsmith',
-            profile: '',
-        }
-    },
-    {
-        url: '',
-        message: '',
-        owner: {
-            name: 'John Smith',
-            handle: 'johnsmith',
-            profile: '',
-        }
-    },
-];
+import { myFetch } from "./my-fetch";
+import session from "./session";
+
+export const getPosts = ()=> myFetch(`posts/byUser/1`);
+
+export function react(post_id){
+    //console.log(session.user)
+    return myFetch('reactions', { Post_id: post_id, Owner_id: 1 })
+}
+
+export function comment(post_id, text){
+    return myFetch('comments', { Post_id: post_id, Owner_id: 1, Text: text })
+}
